9f1a6e1d6c7cff9158b0d15d88981994d1b98d1f
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-21 10:42:55 +0100

    removed the containerized package and replaced it with an environment variable (#21)

--- src/Config.js
+++ src/Config.js
@@ -1,9 +1,9 @@
 const process = require('process');
-const containerized = require('containerized');
 
 class Config {
   constructor() {
-    Config.miraHostName = process.env.MIRA_HOST_NAME || (containerized() ? 'mira' : 'localhost');
+    Config.miraHostName = process.env.MIRA_HOST_NAME ||
+    (process.env.SESSION_SERVICE_CONTAINERIZED && process.env.SESSION_SERVICE_CONTAINERIZED.toLowerCase() === 'true' ? 'mira' : 'localhost');
     Config.miraPort = 9100;
 
     this.port = parseInt(process.env.PORT, 10);
